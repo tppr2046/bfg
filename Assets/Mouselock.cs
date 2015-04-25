f4db261cf97a8ff276e3ebd34d63f90785d906e6
tppr2046 <gopmaster@hotmail.com>
2015-04-25 16:43:00 +0800

    first commit

---
+++ Assets/Mouselock.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class Mouselock : MonoBehaviour {
+
+	public bool isLocked;
+	void Start() {
+
+	}
+
+	void SetCursorLock(bool isLocked)
+	{
+		this.isLocked = isLocked;
+		Screen.lockCursor = isLocked;
+		Cursor.visible = !isLocked;
+	}
+
+
+
+	void Update() {
+		if (Input.GetKeyDown (KeyCode.Escape))
+			SetCursorLock(!isLocked);
+
+		}
+	}
+
